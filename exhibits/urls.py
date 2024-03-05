16f4de855fa80e36a95fd0eabc2ce65111706fbb
jairomelo <jairomelo@gmail.com>
2024-03-05 14:36:18 +0000

    fixed url error

--- exhibits/urls.py
+++ exhibits/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import testing_map, MapExhibitContributionCreateView
 
 urlpatterns = [
-    path("exhibits/syria_recipes_and_cultures", testing_map, name="testing_map"),
+    path("exhibits/syria_recipes_and_cultures/", testing_map, name="testing_map"),
     path("exhibits/syria_recipes_and_cultures/contrib", MapExhibitContributionCreateView.as_view(),
          name='map-contribute')
 ]
