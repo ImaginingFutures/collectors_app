b35c2a022bd830f165b422ba29a5b42f277e1e20
jairomelo <jairomelo@gmail.com>
2024-06-11 12:28:13 +0100

    better url | some cleaning

--- collected/urls.py
+++ collected/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ArticleListView, ArticleDetailView, ArticleDetailHTMLView, ArticleCreateView, ArticleUpdateView
 
 urlpatterns = [
-    path('VolumeB/articles/', ArticleListView.as_view(), name='article_list'),
+    path('VolumeB/', ArticleListView.as_view(), name='article_list'),
     path('VolumeB/articles/<int:pk>/', ArticleDetailView.as_view(), name='article_detail'),
     path('VolumeB/articles/<int:pk>/html/', ArticleDetailHTMLView.as_view(), name='article_detail_html'),
 ]
